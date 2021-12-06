61cc411f57f910d5c59df711fc181cbd47ec3111
devhowey <dev.howey@gmail.com>
2021-12-06 16:33:49 -0600

    set up put route for users

--- api/lib/handlers.js
+++ api/lib/handlers.js
@@ -101,13 +101,57 @@ handlers._users.get = function(data,callback){
         })
     } else {
         callback(400, { 'Error' : 'Missing required field' });
-    }
+     }
     //
 };
 
 // users - put
+// required data: phone
+// optional data: firstName, lastName, password (at least one mus be specified)
+// @TODO set up auth so users can only update their own object, not other users' objects
 handlers._users.put = function(data,callback){
+    // check for required field
+    var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
+    // check for optional fields
+    var firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+    var lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+    var password = typeof(data.payload.password) == 'string' && data.payload.phone.trim().length > 0 ? data.payload.password.trim() : false;
 
+    // error if phone is false
+    if(phone){
+        if(firstName || lastName || password) {
+            // lookup user
+            _data.read('users', phone,function(err,userData){
+                if(!err && userData){
+                    // update necessary fields
+                    if(firstName){
+                        userData.firstName = firstName;
+                    }
+                    if(lastName){
+                        userData.lastName = lastName;
+                    }
+                    if(password){
+                        userData.hashedPassword = helpers.hash(password);
+                    }
+                    // persist and store new data
+                    _data.update('users',phone,userData,function(err){
+                        if(!err){
+                            callback(200);
+                        } else {
+                            console.log(err);
+                            callback(500,{ 'Error' : 'Could not update the user' });
+                        }
+                    })
+                } else {
+                    callback(400, { 'Error' : 'The specified user does not exist' });
+                }
+            });
+        } else {
+            callback(400, {'Error' : 'Missing fields to update'})
+        }
+    } else {
+        callback(400, {'Error' : 'Missing reaquired field'})
+    }
 };
 
 // users - delete
